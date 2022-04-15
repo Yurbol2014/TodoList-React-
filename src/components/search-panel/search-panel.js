17b562031fe62e3715dd680e28a073c2571de74d
Yuriy Bolotin <simplovg@gmail.com>
2022-04-16 02:43:21 +0300

    commit 16.04.22

---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './search-panel.css';
+
+const SearchPanel = () =>{
+   const searchText = 'Type here search';
+   return  (<input className='search-input' placeholder={searchText} />
+      
+      )};
+
+   export default SearchPanel;
