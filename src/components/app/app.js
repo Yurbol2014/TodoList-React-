0d0cd4e52c88fb1953c8da90668e0379eb6f8407
Yuriy Bolotin <simplovg@gmail.com>
2022-04-17 04:57:41 +0300

    редактирование элементов в setState() повышенная сложность

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -59,31 +59,50 @@ export default class App extends Component{
 
    };
 
+   toggleProperty(arr, id, propName){
+      const idx = arr.findIndex((el)=> el.id === id);
+       const oldItem = arr[idx]
+       const newItem = {...oldItem,
+       [propName]: !oldItem[propName]};
+       return [...arr.slice(0,idx),
+         newItem,
+         ...arr.slice(idx +1)];
+     
+   }
+
    onToggleDone = (id) => {
       this.setState(( {todoData} )=>{
-       const idx = todoData.findIndex((el)=> el.id === id);
-       const oldItem = todoData[idx]
-       const newItem = {...oldItem,
-       done: !oldItem.done};
+      return{
+         todoData: this.toggleProperty(todoData, id, 'done')
+      }
       });
    };
 
    onToggleImportant = (id) => {
-      console.log('Toggle Imortant', id)
+      this.setState(( {todoData} )=>{
+         return{
+            todoData: this.toggleProperty(todoData, id, 'important')
+         }
+         });
 
    };
    
-   render ()
-   {
+   render (){
+
+      const {todoData} = this.state;
+      const doneCount = todoData
+                        .filter((el) => el.done).length;
+      const todoCount = todoData.length-doneCount;
+
       return (
          <div className="todo-app">
-           <AppHeader toDo={1} done={3} />
+           <AppHeader toDo={todoCount} done={doneCount} />
            <div className="top-panel d-flex">
              <SearchPanel />
              <ItemStatusFilter />
            </div>
      
-           <TodoList todos={this.state.todoData}
+           <TodoList todos={todoData}
            onDeleted={this.deleteItem}
            onToggleImportant = {this.onToggleImportant}
            onToggleDone={this.onToggleDone}/>
